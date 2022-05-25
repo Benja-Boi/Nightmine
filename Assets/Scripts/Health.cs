6e715a3d54be6cf383de338d398f6eac0e0929e4
ofearos <ophir.benjamin@post.idc.ac.il>
2022-05-25 13:31:24 +0300

    GitHub upload

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Health : MonoBehaviour
+{
+
+    public int maxHealth = 100;
+    public int currentHealth;
+    public float stdDmg = 10f;
+
+    public HealthBar healthBar;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    void Update()
+    {
+  
+    }
+    /*void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log(other.gameObject);
+        if (other.gameObject.tag.Equals("HitPoint"))
+        {
+            int diii;
+            TakeDamage(stdDmg);
+        }
+       
+    }*/
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        //Debug.Log(collider.gameObject);
+        if (collider.gameObject.tag.Equals("HitPoint"))
+        {
+            TakeDamage(stdDmg);
+        }
+        else if (collider.gameObject.tag.Equals("HitPointBoss"))
+        {
+            TakeDamage(stdDmg * 50);
+        }
+        else if (collider.gameObject.tag.Equals("HitPointGoblin"))
+        {
+            TakeDamage(stdDmg * 2);
+        }
+    }
+
+    private void TakeDamage(float stdDmg)
+    {
+        currentHealth -= (int)stdDmg;
+        healthBar.SetHealth(currentHealth);
+        if(currentHealth <= 0)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+
+}
---
+++ Assets/Scripts/sceneChanger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class sceneChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<AudioSource>().Play();
+        StartCoroutine(ExampleCoroutine());
+    }
+    private IEnumerator ExampleCoroutine()
+    {
+        yield return new WaitForSeconds(57);
+        SceneManager.LoadScene("Gameplay");
+    }
+            
+    
+}
